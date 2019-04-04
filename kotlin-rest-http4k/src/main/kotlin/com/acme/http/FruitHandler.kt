ff2c48f1dba86affc9fe947a4fe912f84392c18c
uberto <uberto.gama@gmail.com>
2019-04-04 13:57:27 +0100

    Replaced Http4kServlet with http4k one

--- kotlin-rest-http4k/src/main/kotlin/com/acme/http/FruitHandler.kt
+++ kotlin-rest-http4k/src/main/kotlin/com/acme/http/FruitHandler.kt
@@ -6,21 +6,21 @@ import org.http4k.core.Method.DELETE
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Method.PUT
+import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.ACCEPTED
 import org.http4k.core.Status.Companion.CREATED
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.filter.ServerFilters
-import org.http4k.lens.Path
-import org.http4k.lens.int
-import org.http4k.routing.ResourceLoader.Companion.Classpath
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
-import org.http4k.routing.static
 
 class FruitHandler(val fruits: FruitRepository) {
-    private val id = Path.int().of("id")
+//    private val id = Path.int().of("id")
+
+    private fun id(req: Request) = req.path("id")?.toInt() ?: 0
 
     fun createHandler() =
         ServerFilters.CatchLensFailure
@@ -28,11 +28,15 @@ class FruitHandler(val fruits: FruitRepository) {
                 routes(
                     "fruits" bind routes(
                         "/" bind GET to {
-                            Response(OK).body(fruits.getAll().toString())
+                            val fruits = fruits.getAll().toJson()
+                            println("!! ${it.bodyString()}  $fruits")
+
+
+                            Response(OK).body(fruits)
                         },
                         "/" bind POST to {
-                            println("!!")
-                            fruits.addFruit(it.bodyString())
+                            val fruitName = it.bodyString().getName()
+                            fruits.addFruit(fruitName)
                             Response(CREATED)
                         },
                         "/{id}" bind GET to {
@@ -50,7 +54,7 @@ class FruitHandler(val fruits: FruitRepository) {
                             Response(ACCEPTED)
                         }
                     ),
-                    "/" bind GET to {
+                    "" bind GET to {
                         val resource = FruitHandler::class.java.getResource("/static/index.html")
                         Response(OK).body(resource.openStream())
                     }
@@ -58,3 +62,10 @@ class FruitHandler(val fruits: FruitRepository) {
                 )
             )
 }
+
+private fun List<Fruit>.toJson(): String =
+    "[" +
+    this.map {""" { "id": "${it.id}", "name": "${it.name}" } """ }.joinToString(",") +
+            "]"
+
+private fun String.getName(): String = this.substring(9, this.length - 2)
