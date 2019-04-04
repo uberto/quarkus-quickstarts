8b76adf5ca2a50ca7f40ae0ae49f2e165149e19d
uberto <uberto.gama@gmail.com>
2019-04-04 13:57:27 +0100

    Replaced Http4kServlet with http4k one

--- kotlin-rest-http4k/src/main/kotlin/com/acme/http/RestServlet.kt
+++ kotlin-rest-http4k/src/main/kotlin/com/acme/http/RestServlet.kt
@@ -1,9 +1,9 @@
 package com.acme.http
 
+import org.http4k.servlet.HttpHandlerServlet
 import javax.servlet.Servlet
 import javax.servlet.annotation.WebServlet
 
 
-
 @WebServlet("/")
-open class RestServlet: Servlet by Http4kServlet(fruitsHandler)
+open class RestServlet: Servlet by HttpHandlerServlet(fruitsHandler)
